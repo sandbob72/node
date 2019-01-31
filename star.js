cf6b0369f606d297d0301b3f912061ef014b2039
sakul <sakul.s2557@gmail.com>
2019-01-31 20:26:21 +0700

    add star

--- star.js
+++ star.js
@@ -1,11 +1,60 @@
 module.exports.starS = () =>{
-    var i=0, j=0
-    for(i=0;i<5;i++){
-        for(j=i;j<5;j++){
-            console.log("*");
-            
-        }
-        // console.log();
+    // stdin =process.openStdin()
+// stdin.addListener("data",(d)=>{
+
+// })
+var num = ''
+var num2 = ''
+
+// for(var i=0 ; i<5 ; i++){
+    
+//     for(var j = 5-i ; j>0 ; j--){
+//         num = num+'+'
+//         // console.log(i);
         
-    }
+//     }
+//     for(var k=0 ; k<=i ; k++){
+//         num = num + '*'
+//     }
+     
+//     console.log(num);
+//     num = ''
+       
+// }
+// console.log("ok");
+
+//------------------------------------------------------
+
+// for(var i=0 ; i<5 ; i++){
+//         num = num+'*'
+//         // console.log(i);
+//     console.log(num);   
+// }
+// console.log("ok");
+
+//------------------------------------------------------
+
+// for(var i=0 ; i<5 ; i++){
+    
+//     for(var j = 5-i ; j>0 ; j--){
+//         num = num+'*'
+//         // console.log(i);   
+//     }
+     
+//     console.log(num);
+//     num = ''
+       
+// }
+// console.log("ok");
+
+//-----------------------------------------------------
+
+for(var i=0 ; i<5 ; i++){
+        num = num+'*'
+        // console.log(i);
+    console.log(num);    
+}
+console.log("ok");
+
+//---------------------------------------------------
 }
